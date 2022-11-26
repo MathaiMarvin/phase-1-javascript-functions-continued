dd99a8528dbf0c051f498ecbf13a6344d5656599
m-a-t-h-a-i <mathaimarvin574@gmail.com>
2022-11-26 23:03:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // code your solution here
+function saturdayFun(fun="roller-skate"){
+    return `This Saturday, I want to ${fun}!`
+}
+saturdayFun("bathe my dog")
+
+function mondayWork(work = "go to the office"){
+    return  `This Monday, I will ${work}.`
+}
+mondayWork("work from home");
+
+function wrapAdjective(flair = '*'){
+    return function emphatic(now="special"){
+        return `You are ${flair}${now}${flair}!`
+    }
+}
+wrapAdjective("||")("dedicated programmer")
